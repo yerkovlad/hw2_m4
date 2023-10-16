83108ad0d2ecd5c13c2e2b00aec28508b97df41d
yerkovlad <61785699+yerkovlad@users.noreply.github.com>
2023-10-16 10:54:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import json
 from jinja2 import Environment, FileSystemLoader
 import os
 from datetime import datetime
+import socket
+import threading
 
 PORT_HTTP = 3000
+PORT_SOCKET = 5000
 
-templates_env = Environment(loader=FileSystemLoader('templates')
+templates_env = Environment(loader=FileSystemLoader('templates'))
 
 class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -37,27 +40,21 @@ class MyHandler(BaseHTTPRequestHandler):
             content_length = int(self.headers['Content-Length'])
             post_data = self.rfile.read(content_length)
             data = json.loads(post_data.decode())
-    
+            template = templates_env.get_template('message.html')
             timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+
             save_data({
                 timestamp: {
                     "username": data["username"],
                     "message": data["message"]
                 }
             })
-
-            send_data_to_socket({
-                "timestamp": timestamp,
-                "data": data
-            })
-    
-            template = templates_env.get_template('message.html')
             response_data = {
                 "message": data,
                 "timestamp": timestamp
             }
             response = template.render(response_data)
-    
+
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
@@ -66,20 +63,47 @@ class MyHandler(BaseHTTPRequestHandler):
 def save_data(data):
     data_file = 'storage/data.json'
     os.makedirs(os.path.dirname(data_file), exist_ok=True)
-    with open(data_file, 'a') as file:
-        json.dump(data, file, indent=4)
-        file.write('\n')
 
-def send_data_to_socket(data):
+    if not os.path.exists(data_file):
+        # Якщо файл не існує, створіть новий список даних
+        data_list = []
+    else:
+        # Якщо файл існує, завантажте вміст
+        with open(data_file, 'r') as file:
+            data_list = json.load(file)
+
+    # Додайте новий словник даних до списку
+    data_list.append(data)
+
+    # Запишіть список у файл JSON, перезаписавши його
+    with open(data_file, 'w') as file:
+        json.dump(data_list, file, indent=4)
+
+
+def socket_server():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_address = ('localhost', PORT_SOCKET)
+    sock.bind(server_address)
+    print(f"Socket server is running on {server_address[0]}:{server_address[1]}")
+
     try:
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-            server_address = ('localhost', PORT_SOCKET)
-            message = json.dumps(data)
-            sock.sendto(message.encode('utf-8'), server_address)
-    except Exception as e:
-        print(f"Помилка відправки даних до сокет-серверу: {str(e}")
+        while True:
+            data, address = sock.recvfrom(1024)
+            data_dict = json.loads(data.decode())
+            save_data({
+                datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'): data_dict
+            })
+    except KeyboardInterrupt:
+        print("Stopping socket server")
+    finally:
+        sock.close()
+
+socket_thread = threading.Thread(target=socket_server)
+socket_thread.daemon = True
+socket_thread.start()
 
 if __name__ == '__main__':
     http_server = HTTPServer(('localhost', PORT_HTTP), MyHandler)
     print(f"HTTP server is running on port {PORT_HTTP}")
     http_server.serve_forever()
+
